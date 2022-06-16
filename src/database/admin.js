ecf35ffa10d048f3b7c37586e0cbe25b7ba54062
ppratham0811 <ppratham0811@gmail.com>
2022-06-16 09:07:23 +0530

    admin schema

---
+++ src/database/admin.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+const bcrypt = require("bcrypt");
+
+const adminSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+adminSchema.statics.findAndValidate = async function (username, password) {
+    const foundUser = await this.findOne({ username });
+    if (foundUser) {
+        const isValid = await bcrypt.compare(password, foundUser.password);
+        return isValid ? foundUser : false;
+    } else {
+        return false;
+    }
+};
+
+adminSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
+
+const Admin = model("Admin", adminSchema);
+
+module.exports = Admin;
