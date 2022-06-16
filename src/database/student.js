e2e93a55df47f734b8ee81d04b27f9620c56ce71
ppratham0811 <ppratham0811@gmail.com>
2022-06-16 09:07:39 +0530

    student schema

---
+++ src/database/student.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+const bcrypt = require("bcrypt");
+
+const studentSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    subjectScore: [
+        {
+            subject: String,
+            score: Number,
+        },
+    ],
+    classId: {
+        type: Schema.Types.ObjectId,
+        ref: "Class",
+    },
+});
+
+studentSchema.statics.findAndValidate = async function (username, password) {
+    const foundUser = await this.findOne({ username });
+    if (foundUser) {
+        const isValid = await bcrypt.compare(password, foundUser.password);
+        return isValid ? foundUser : false;
+    } else {
+        return false;
+    }
+};
+
+studentSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
+
+const Student = model("Student", studentSchema);
+module.exports = Student;
