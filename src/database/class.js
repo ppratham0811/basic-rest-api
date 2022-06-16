be803806cb7f196a69a38c8c8717e937e0f1afb3
ppratham0811 <ppratham0811@gmail.com>
2022-06-16 09:13:29 +0530

    class schema

---
+++ src/database/class.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const {Schema, model} = mongoose;
+
+const classSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    teachers: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Teacher',
+    }],
+    students: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Student',
+    }]
+});
+
+const Class = model('Class',classSchema);
+module.exports = Class;
