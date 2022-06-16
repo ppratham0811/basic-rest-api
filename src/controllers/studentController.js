2a1d05d7c5e79d3671b9e24b6fea915477c97b72
ppratham0811 <ppratham0811@gmail.com>
2022-06-16 22:04:29 +0530

    added display marks controller

--- src/controllers/studentController.js
+++ src/controllers/studentController.js
@@ -1,8 +1,10 @@
 const Student = require("../database/student");
 const getMarks = async (req, res, next) => {
-    const student = req.session.user;
-    let allMarks = await Student.findById(student._id);
-    res.send(`Welcome, ${student.name}\n\nAll grades:\n${allMarks}`);
+    const studentId = req.session.user._id;
+    let student = await Student.findById(studentId);
+    res.send(
+        `Welcome, ${student.name}\nRoll no.: ${student.roll}\n\nAll grades:\n${student.scoreCard}`
+    );
 };
 
 module.exports = {
