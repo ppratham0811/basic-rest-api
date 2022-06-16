eee0cb128a1bab5b9fb92fed4e4fbb0155c73928
ppratham0811 <ppratham0811@gmail.com>
2022-06-16 09:12:26 +0530

    view marks routes

---
+++ src/routes/studentRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const studentController = require("../controllers/studentController");
+const catchAsync = require('../utils/catchAsync');
+
+const { isLoggedIn, studentLoggedIn } = require("../middlewares");
+
+router.get(
+    "/",
+    isLoggedIn,
+    studentLoggedIn,
+    studentController.getMarks
+);
+
+module.exports = router;
