92c1ee66b8ba8424055e4c3b5b9df82ce63651ce
ppratham0811 <ppratham0811@gmail.com>
2022-06-16 09:07:52 +0530

    teacher schema

---
+++ src/database/teacher.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+const bcrypt = require("bcrypt");
+
+const teacherSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    classId: {
+        type: Schema.Types.ObjectId,
+        ref: "Class",
+    },
+});
+
+teacherSchema.statics.findAndValidate = async function (username, password) {
+    const foundUser = await this.findOne({ username });
+    if (foundUser) {
+        const isValid = await bcrypt.compare(password, foundUser.password);
+        return isValid ? foundUser : false;
+    } else {
+        return false;
+    }
+};
+
+teacherSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
+
+const Teacher = model("Teacher", teacherSchema);
+module.exports = Teacher;
